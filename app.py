5eefd6f3dcab94bb666a72b6d751ffb2ca3fd38f
Bxxm29 <66027157@up.ac.th>
2025-08-20 08:46:49 +0000

    iris app

--- app.py
+++ app.py
@@ -7,6 +7,5 @@ import seaborn as sns
 from sklearn.cluster import KMeans 
 from sklearn.metrics import silhouette_score
 from sklearn.preprocessing import MinMaxScaler
-
 iris = pd.read_csv("IRIS.csv")
 x = iris.iloc[:, [0, 1, 2, 3]].values
