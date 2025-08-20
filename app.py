61f50d4e45fa63ec2b6018979873fef385781aab
Bxxm29 <66027157@up.ac.th>
2025-08-20 08:09:18 +0000

    df

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+
+st.write('สวัสดี นภสร')
+df = pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+    })
+df
